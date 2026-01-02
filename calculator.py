35ccd894da35e4b4c386b365fe8bc9c307d76c63
Kauã <kauachujo@gmail.com>
2026-01-02 13:27:40 -0300

    Style update

--- calculator.py
+++ calculator.py
@@ -1,31 +1,39 @@
-menu = input(' A. Addition\n B. Subtraction\nenter the number: ').lower()
+menu = input(
+    'A. Addition\n'
+    'B. Subtraction\n'
+    'Enter the option: '        
+).strip().lower()
 
 if menu == 'a':
     addition_list = list()
     while True:
         number = input('Add a number (C to cancel): ')
+
         try:
             number = int(number)
             addition_list.append(number)
         except:
             if number.lower()[0] == 'c':
                 break
+
         print(f'Result: {sum(addition_list)}')
 
 elif menu == 'b':
     subtraction_list = list()
     while True:
-        number2 = input('Add a number (C to cancel): ')
+        number = input('Add a number (C to cancel): ')
+
         try:
-            number2 = int(number2)
-            subtraction_list.append(number2)
+            number = int(number)
+            subtraction_list.append(number)
         except:
-            if number2.lower()[0] == 'c':
+            if number.lower()[0] == 'c':
                 break
 
         result = subtraction_list[0]
         for n in subtraction_list[1:]:
             result -= n
+
         print(f'Result: {result}')
 
     
