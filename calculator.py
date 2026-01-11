123e935c6e2c9c696c4c6e289c28bb395a5a5479
Kauã <kauachujo@gmail.com>
2026-01-11 19:24:34 -0300

    Simplify button creation

--- calculator.py
+++ calculator.py
@@ -14,39 +14,38 @@ entry.grid(row=0, column=0, sticky='', pady=10)
 # BOTÕES
 
 frame = tk.Frame(window)
-frame.grid(row=1, column=0, pady=20)
+frame.grid(row=1, column=0, pady=5)
 
-bt0 = tk.Button(frame, text='0', width=2, height=1, font=('Arial', 20))
-bt1 = tk.Button(frame, text='1', width=2, height=1, font=('Arial', 20))
-bt2 = tk.Button(frame, text='2', width=2, height=1, font=('Arial', 20))
-bt3 = tk.Button(frame, text='3', width=2, height=1, font=('Arial', 20))
-bt4 = tk.Button(frame, text='4', width=2, height=1, font=('Arial', 20))
-bt5 = tk.Button(frame, text='5', width=2, height=1, font=('Arial', 20))
-bt6 = tk.Button(frame, text='6', width=2, height=1, font=('Arial', 20))
-bt7 = tk.Button(frame, text='7', width=2, height=1, font=('Arial', 20))
-bt8 = tk.Button(frame, text='8', width=2, height=1, font=('Arial', 20))
-bt9 = tk.Button(frame, text='9', width=2, height=1, font=('Arial', 20))
-btA = tk.Button(frame, text='+', width=2, height=1, font=('Arial', 20))
-btS = tk.Button(frame, text='-', width=2, height=1, font=('Arial', 20))
-btM = tk.Button(frame, text='/', width=2, height=1, font=('Arial', 20))
-btD = tk.Button(frame, text='x', width=2, height=1, font=('Arial', 20))
-btI = tk.Button(frame, text='=', width=2, height=1, font=('Arial', 20))
+symbol_list = [
+    'C', None, None, '⌫',
+    '9', '8', '7', '+',
+    '6', '5', '4', '-',
+    '3', '2', '1', 'x',
+    None, '0', None, '/'
+]
 
-bt0.grid(row=4, column=2, padx=8, pady=8)
-bt1.grid(row=1, column=1, padx=8, pady=8)
-bt2.grid(row=1, column=2, padx=8, pady=8)
-bt3.grid(row=1, column=3, padx=8, pady=8)
-bt4.grid(row=2, column=1, padx=8, pady=8)
-bt5.grid(row=2, column=2, padx=8, pady=8)
-bt6.grid(row=2, column=3, padx=8, pady=8)
-bt7.grid(row=3, column=1, padx=8, pady=8)
-bt8.grid(row=3, column=2, padx=8, pady=8)
-bt9.grid(row=3, column=3, padx=8, pady=8)
-btA.grid(row=1, column=4, padx=8, pady=8)
-btS.grid(row=2, column=4, padx=8, pady=8)
-btM.grid(row=3, column=4, padx=8, pady=8)
-btD.grid(row=4, column=1, padx=8, pady=8)
-btI.grid(row=4, column=3, padx=8, pady=8)
+button_list = []
+
+row = 1
+column = 1
+
+for i, symbol in enumerate(symbol_list):
+    if symbol != None:
+        button = tk.Button(frame, text=symbol, width=2, height=1, font=('Arial', 20))
+        button.grid(row=row, column=column, padx=5, pady=5)
+        print(symbol, row, column)
+        button_list.append(button)
+
+    column += 1
+    if column > 4:
+        column = 1
+        row += 1
+    
+
+
+#bt0 = tk.Button(frame, text='0', width=2, height=1, font=('Arial', 20))
+#bt0.grid(row=4, column=2, padx=8, pady=8)
+
 
 
 window.mainloop()
