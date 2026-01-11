9554229a38ca33624b2b4e5c8faa258c15f56efc
Kauã <kauachujo@gmail.com>
2026-01-11 20:14:12 -0300

    Added button functions
    Now it's possible to calculate the result of the expression

--- calculator.py
+++ calculator.py
@@ -17,35 +17,55 @@ frame = tk.Frame(window)
 frame.grid(row=1, column=0, pady=5)
 
 symbol_list = [
-    'C', None, None, '⌫',
-    '9', '8', '7', '+',
-    '6', '5', '4', '-',
-    '3', '2', '1', 'x',
-    None, '0', None, '/'
+    'C', '+', '-', '⌫',
+    '9', '8', '7', '×',
+    '6', '5', '4', '÷',
+    '3', '2', '1', '=',
+    None, '0', None, None
 ]
 
 button_list = []
-
 row = 1
 column = 1
 
+def on_button_click(symbol):
+    if symbol.isdigit():
+        entry.insert(tk.END, symbol)
+
+    elif symbol == 'C':
+        entry.delete(0, tk.END)
+
+    elif symbol == '⌫':
+        text = entry.get()
+        entry.delete(0, tk.END)
+        entry.insert(0, text[:-1])
+    
+    elif symbol == '=':
+        expression = entry.get().replace('×', '*')
+        expression = expression.replace('÷', '/')
+        result = str(eval(expression))
+        entry.delete(0, tk.END)
+        entry.insert(0, result)
+    else:
+        entry.insert(tk.END, symbol)
+
+
 for i, symbol in enumerate(symbol_list):
     if symbol != None:
-        button = tk.Button(frame, text=symbol, width=2, height=1, font=('Arial', 20))
+        button = tk.Button(frame, text=symbol, width=2, height=1, font=('Arial', 20),
+                           command=lambda s=symbol: on_button_click(s))
         button.grid(row=row, column=column, padx=5, pady=5)
-        print(symbol, row, column)
         button_list.append(button)
 
     column += 1
     if column > 4:
         column = 1
         row += 1
+
+for i, button in enumerate(button_list):
+    grid = button.grid_info()
+    if grid['row'] == 1 or grid['column'] == 4:
+        button.config(bg='orange')
     
-
-
-#bt0 = tk.Button(frame, text='0', width=2, height=1, font=('Arial', 20))
-#bt0.grid(row=4, column=2, padx=8, pady=8)
-
-
-
+print(button_list)
 window.mainloop()
